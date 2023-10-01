18402c52b09570fdebb73ae41fec75aa964983d8
Astrid Eldhuset <astrid.eldhuset@helsedir.no>
2023-10-01 08:26:17 +0200

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+// This project was created following the guide at https://kotlinlang.org/docs/jvm-get-started.html
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
